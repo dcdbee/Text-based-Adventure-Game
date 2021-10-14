592cff25ed3c4e41c004edfbe31e716a1ec7c024
dcdb <79600718+dcdbee@users.noreply.github.com>
2021-10-14 10:15:12 +0100

    stickman thing

--- Text based Adventure Game/Program.cs
+++ Text based Adventure Game/Program.cs
@@ -61,6 +61,16 @@ namespace Text_based_Adventure_Game
 
 ";
 
+        static string Stickman = @"
+                                                                                                                        
+                                                                                                          .    *    _@   .    .
+                                                                                                         .  o   .  </\_   o  *
+                                                                                                           .     ~\/\  '      o
+                                                                                                         o     .    /   .  *
+                                                                                                                    ~
+                                                                                                                
+";
+
         #endregion        
         static void Main()
         {
@@ -80,21 +90,22 @@ namespace Text_based_Adventure_Game
             EnemyMaxHealth = 20;
             int EnemyDamage;
             Random RND = new Random();
-            while(BattleEnd == false){
-            UpdateScreen();
-            Console.WriteLine("Press any key to attack");
-            Console.ReadKey();
-            EnemyHealth = EnemyHealth - RND.Next(1,5);
-            UpdateScreen();
-            CheckDeath();
-            if(BattleEnd) {break;}
-            Console.ReadKey();
-            EnemyDamage = RND.Next(1,5);
-            Health = Health - EnemyDamage;
-            UpdateScreen();
-            Console.WriteLine("The enemy dealt " + EnemyDamage);
-            CheckDeath();
-            Console.ReadKey();
+            while (BattleEnd == false)
+            {
+                UpdateScreen();
+                Console.WriteLine("Press any key to attack");
+                Console.ReadKey();
+                EnemyHealth = EnemyHealth - RND.Next(1, 5);
+                UpdateScreen();
+                CheckDeath();
+                if (BattleEnd) { break; }
+                Console.ReadKey();
+                EnemyDamage = RND.Next(1, 5);
+                Health = Health - EnemyDamage;
+                UpdateScreen();
+                Console.WriteLine("The enemy dealt " + EnemyDamage);
+                CheckDeath();
+                Console.ReadKey();
             }
             Console.ReadKey();
             Console.Clear();
@@ -104,7 +115,7 @@ namespace Text_based_Adventure_Game
 
         static void CheckDeath()
         {
-            if(EnemyHealth <= 0)
+            if (EnemyHealth <= 0)
             {
                 Console.Write("Enemy died you win");
                 BattleEnd = true;
@@ -127,14 +138,16 @@ namespace Text_based_Adventure_Game
 
                                                                 ");
             Console.Write("                  ");
-            if(Health > MaxHealth){ Health = MaxHealth;
-}
-            for(int i = 0; i < Health; i++)
+            if (Health > MaxHealth)
+            {
+                Health = MaxHealth;
+            }
+            for (int i = 0; i < Health; i++)
             {
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.Write("█");
             }
-            for(int j = 0; j < MaxHealth - Health; j++)
+            for (int j = 0; j < MaxHealth - Health; j++)
             {
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 Console.Write("█");
@@ -150,12 +163,12 @@ namespace Text_based_Adventure_Game
 
                                                                 ");
             Console.Write("                  ");
-            for(int i = 0; i < EnemyHealth; i++)
+            for (int i = 0; i < EnemyHealth; i++)
             {
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.Write("█");
             }
-            for(int j = 0; j < EnemyMaxHealth - EnemyHealth; j++)
+            for (int j = 0; j < EnemyMaxHealth - EnemyHealth; j++)
             {
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 Console.Write("█");
@@ -165,7 +178,7 @@ namespace Text_based_Adventure_Game
         }
 
         static void Menu()
-        { 
+        {
             Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine(Title);
             Console.ForegroundColor = ConsoleColor.Gray;
@@ -174,15 +187,15 @@ namespace Text_based_Adventure_Game
             string UserInput;
             Console.Write("Input: ");
             UserInput = Console.ReadLine();
-            while (UserInput != "1" && UserInput != "2") 
-            { 
-                Cosmetic("text", "ERROR: ", 25, true, true, ConsoleColor.DarkRed); 
+            while (UserInput != "1" && UserInput != "2")
+            {
+                Cosmetic("text", "ERROR: ", 25, true, true, ConsoleColor.DarkRed);
                 Cosmetic("text", "Please enter either 1 or 2", 25, true, true, ConsoleColor.Gray);
                 Console.Write("Input: ");
                 UserInput = Console.ReadLine();
             }
             Cosmetic("text", "INPUT ACCEPTED", 25, true, true, ConsoleColor.Green);
-            if(UserInput == "2") 
+            if (UserInput == "2")
             {
                 QuitGame();
             }
@@ -190,22 +203,22 @@ namespace Text_based_Adventure_Game
             {
                 Cosmetic("text", "Do you wish to enter a username (1) or have one randomly generated (2)", 25, true, true, ConsoleColor.Magenta);
                 UserInput = Console.ReadLine();
-                while(UserInput != "1" && UserInput != "2")
+                while (UserInput != "1" && UserInput != "2")
                 {
-                    Cosmetic("text", "ERROR: ", 25, true, true, ConsoleColor.DarkRed); 
+                    Cosmetic("text", "ERROR: ", 25, true, true, ConsoleColor.DarkRed);
                     Cosmetic("text", "Please enter either 1 or 2", 25, true, true, ConsoleColor.Gray);
                     Console.Write("Input: ");
                     UserInput = Console.ReadLine();
                 }
                 Cosmetic("text", "INPUT ACCEPTED", 25, true, true, ConsoleColor.Green);
-                if(UserInput == "1")
+                if (UserInput == "1")
                 {
                     Cosmetic("text", "Please enter a username!", 25, true, true, ConsoleColor.Magenta);
                     Console.WriteLine("Input: ");
                     PlayerName = Console.ReadLine();
-                    while(PlayerName == null)
+                    while (PlayerName == null)
                     {
-                        Cosmetic("text", "ERROR: ", 25, true, true, ConsoleColor.DarkRed); 
+                        Cosmetic("text", "ERROR: ", 25, true, true, ConsoleColor.DarkRed);
                         Cosmetic("text", "Please enter a valid username", 25, true, true, ConsoleColor.Gray);
                         Console.Write("Input: ");
                         PlayerName = Console.ReadLine();
@@ -217,7 +230,6 @@ namespace Text_based_Adventure_Game
                 else
                 {
                     GenerateName();
-                    StartGame();
                 }
             }
             Console.ReadKey();
@@ -226,9 +238,9 @@ namespace Text_based_Adventure_Game
         static void GenerateName()
         {
             Random RND = new Random();
-            string[] Adjectives = {"Adorable", "Agreeable", "Amused", "Annoying", "Ashamed", "Awful", "Bloody", "Blushing", "Brave", "Busy", "Cautious", "Clean", "Condemned", "Courageous", "Crowded", "Doubtful", "Faithful", "Fantastic", "Fine",};
-            string[] Nouns = {"Actor", "Airport", "Animal", "Battery", "Beach", "Army", "Bed", "Boy", "Camera", "Candle", "Carpet", "Melon", "Crayon", "Daughter", "Dog", "Diamond", "Elephant", "Engine", "Footbal", "Fountain", "Forest", "Furniture", "Garage",};
-            PlayerName = Adjectives[RND.Next(1, Adjectives.Length)] + Nouns[RND.Next(1,Nouns.Length)] + RND.Next(100,1000);
+            string[] Adjectives = { "Adorable", "Agreeable", "Amused", "Annoying", "Ashamed", "Awful", "Bloody", "Blushing", "Brave", "Busy", "Cautious", "Clean", "Condemned", "Courageous", "Crowded", "Doubtful", "Faithful", "Fantastic", "Fine", };
+            string[] Nouns = { "Actor", "Airport", "Animal", "Battery", "Beach", "Army", "Bed", "Boy", "Camera", "Candle", "Carpet", "Melon", "Crayon", "Daughter", "Dog", "Diamond", "Elephant", "Engine", "Footbal", "Fountain", "Forest", "Furniture", "Garage", };
+            PlayerName = Adjectives[RND.Next(1, Adjectives.Length)] + Nouns[RND.Next(1, Nouns.Length)] + RND.Next(100, 1000);
             Cosmetic("text", "Your randomly selected name is " + PlayerName, 25, true, true, ConsoleColor.Magenta);
             StartGame();
         }
@@ -236,7 +248,15 @@ namespace Text_based_Adventure_Game
         static void StartGame()
         {
             Console.ReadKey();
-            Console.WriteLine("game");
+            Console.Clear();
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write(@"
+
+
+                                                                                                            " + PlayerName);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(Stickman);
         }
 
         static void QuitGame()
@@ -258,7 +278,7 @@ namespace Text_based_Adventure_Game
             Console.ForegroundColor = colour;
             if (type == "text")
             {
-                if(centered) {Console.SetCursorPosition((Console.WindowWidth - text.Length) / 2, Console.CursorTop);}
+                if (centered) { Console.SetCursorPosition((Console.WindowWidth - text.Length) / 2, Console.CursorTop); }
                 else { Console.SetCursorPosition(0, 0); }
                 for (int i = 0; i < text.Length; i++)
                 {
@@ -268,7 +288,7 @@ namespace Text_based_Adventure_Game
                 if (NextLine) { Console.WriteLine(" "); }
             }
 
-            else if(type == "Ascii")
+            else if (type == "Ascii")
             {
                 Console.WriteLine(text);
             }
